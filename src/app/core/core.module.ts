99ec28f4e51e7f12757b75cf494c65a58137857b
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-24 11:23:24 +0100

    Core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { IconsProviderModule } from '../icons-provider.module';
+import { NZ_I18N, nl_NL } from 'ng-zorro-antd';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import nl from '@angular/common/locales/nl';
 import { ShellComponent } from './components/shell/shell.component';
+import { SharedModule } from '../shared';
 
-
+registerLocaleData(nl);
 
 @NgModule({
   declarations: [ShellComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    BrowserModule,
+    IconsProviderModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    SharedModule
+  ],
+  providers: [{ provide: NZ_I18N, useValue: nl_NL }],
+  exports: [ShellComponent]
 })
 export class CoreModule { }
