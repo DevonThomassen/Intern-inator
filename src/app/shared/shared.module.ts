345b3c1e84de04200a75d81657a7e3f59c4e30ea
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-24 11:23:06 +0100

    Shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, NgZorroAntdModule],
+  exports: [NgZorroAntdModule]
 })
 export class SharedModule { }
