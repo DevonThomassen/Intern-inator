5adfde097c71b0c2213b723c5b4a9f5123ca50bd
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-24 16:32:55 +0100

    Auth service

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { ReplaySubject, BehaviorSubject, Observable } from 'rxjs';
+import { User, ICredentials, IUser } from 'src/app/models';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private currentUser: ReplaySubject<User> = new ReplaySubject(1);
+  private currentToken: BehaviorSubject<string> = new BehaviorSubject(null);
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  public getUser() {
+    return this.currentUser;
+  }
+
+  public getToken() {
+    return this.currentToken.getValue();
+  }
+
+  public isAuthenticated(): Observable<boolean> {
+    return this.currentUser.pipe(
+      map(user => {
+        return !!user && !!this.getToken();
+      })
+    );
+
+  }
+
+  public login(credentials: ICredentials): Observable<User> {
+    return this.http.post<IUser>('login', credentials)
+  }
+
+  public logout() {
+
+  }
 }
