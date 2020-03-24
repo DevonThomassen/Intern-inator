0af95c855cf924347e2ee1ce4c3ef95113b436f1
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-24 16:32:25 +0100

    Routing with auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShellComponent } from './core/components/shell/shell.component';
+import { AuthenticatedGuard } from './core/guards/authenticated.guard';
 
 const routes: Routes = [
-  { path: '', component: ShellComponent },
+  { path: '', component: ShellComponent, canActivate: [AuthenticatedGuard], },
   { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
-  { path: '**', redirectTo: ''}
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
