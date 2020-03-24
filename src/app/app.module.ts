7709cba067ceddd4532fa32f6caf4239800e81dc
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-24 11:23:16 +0100

    App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './core';
 import { AppComponent } from './app.component';
-import { IconsProviderModule } from './icons-provider.module';
-import { NgZorroAntdModule, NZ_I18N, nl_NL } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
-import nl from '@angular/common/locales/nl';
-
-registerLocaleData(nl);
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    IconsProviderModule,
-    NgZorroAntdModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule
-  ],
-  providers: [{ provide: NZ_I18N, useValue: nl_NL }],
+  declarations: [ AppComponent ],
+  imports: [CoreModule, AppRoutingModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
