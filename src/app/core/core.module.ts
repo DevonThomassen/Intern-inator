d77e914505f7d17d39e249a68223c93253e20b18
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-25 11:38:45 +0100

    Providers in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { IconsProviderModule } from '../icons-provider.module';
 import { NZ_I18N, nl_NL } from 'ng-zorro-antd';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import nl from '@angular/common/locales/nl';
 import { ShellComponent } from './components/shell/shell.component';
 import { SharedModule } from '../shared';
+import { environment } from 'src/environments/environment';
+import { BaseInterceptor } from './interceptors/base.interceptor';
 
 registerLocaleData(nl);
 
@@ -24,7 +26,11 @@ registerLocaleData(nl);
     RouterModule,
     SharedModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: nl_NL }],
+  providers: [
+    { provide: NZ_I18N, useValue: nl_NL },
+    { provide: 'BASE_API_URL', useValue: environment.API_URL },
+    { provide: HTTP_INTERCEPTORS, useClass: BaseInterceptor, multi: true },
+  ],
   exports: [ShellComponent]
 })
 export class CoreModule { }
