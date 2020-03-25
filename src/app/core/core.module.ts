5cb8332d98b13be8707967f85da005c29498d85e
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-25 15:45:41 +0100

    Add interceptor in core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -11,7 +11,7 @@ import nl from '@angular/common/locales/nl';
 import { ShellComponent } from './components/shell/shell.component';
 import { SharedModule } from '../shared';
 import { environment } from 'src/environments/environment';
-import { BaseInterceptor } from './interceptors/base.interceptor';
+import { BaseInterceptor, TokenInterceptor } from './interceptors';
 
 registerLocaleData(nl);
 
@@ -30,6 +30,7 @@ registerLocaleData(nl);
     { provide: NZ_I18N, useValue: nl_NL },
     { provide: 'BASE_API_URL', useValue: environment.API_URL },
     { provide: HTTP_INTERCEPTORS, useClass: BaseInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }
   ],
   exports: [ShellComponent]
 })
