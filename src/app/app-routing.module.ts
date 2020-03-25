831ebcb27f2990c7f49f10ca504fec571bce11c4
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-25 15:44:56 +0100

    Add guards to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShellComponent } from './core/components/shell/shell.component';
-import { AuthenticatedGuard } from './core/guards/authenticated.guard';
+import { AuthenticatedGuard, LoggedInGuard } from './core/guards';
 
 const routes: Routes = [
-  { path: '', component: ShellComponent, canActivate: [AuthenticatedGuard], },
-  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  {
+    path: 'auth',
+    canActivate: [LoggedInGuard],
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: '',
+    component: ShellComponent,
+    canActivate: [AuthenticatedGuard],
+    children: [
+      {
+        path: 'pages',
+        loadChildren: () => import('./modules/pages/pages.module').then(m => m.PagesModule)
+      },
+      {
+        path: '**',
+        redirectTo: 'pages'
+      }
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
 
