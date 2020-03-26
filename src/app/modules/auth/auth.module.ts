e0d27b77ac7d3557aa790d6f58bba163141c3b02
DevonThomassen <devon.thomassen@hotmail.com>
2020-03-26 12:31:09 +0100

    Import Auth module

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './components/login';
+import { LoginComponent } from './components';
 import { SharedModule } from 'src/app/shared';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
